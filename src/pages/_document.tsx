54c8359ad0066f16916e4764234893a2b2086209
Fernando Ely <luisely@gmail.com>
2022-02-24 19:28:43 -0300

    them color

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,7 +6,8 @@ export default class MyDocument extends Document {
             <Html>
                 <Head>
                     <link rel="preconnect" href="https://fonts.gstatic.com" />
-                    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&family=Roboto:ital,wght@0,400;0,500;0,700;1,500&display=swap" rel="stylesheet" /> 
+                    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&family=Roboto:ital,wght@0,400;0,500;0,700;1,500&display=swap" rel="stylesheet" />
+                    <meta name="theme-color" content="#181818"></meta>
                 </Head>
                 <body>
                     <Main />
